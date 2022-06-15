d50f5e3bd96e31d819fa306b8efbd8f66faacae2
Tristan Hinsley <thinsley@protonmail.com>
2022-06-15 10:22:45 -0400

    Added nojekyll and changed config

--- homelab-wiki/docusaurus.config.js
+++ homelab-wiki/docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Hinsley Homelab Wiki',
+  tagline: 'Teamwork makes the dreamwork',
+  url: 'https://thinsley.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'thinsley', // Usually your GitHub org/user name.
+  projectName: 'homelab-wiki', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
